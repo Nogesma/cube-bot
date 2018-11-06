e2640551501866378ef6bc67598c11490432fcbb
Maxime ROBIN <contact.waxo@gmail.com>
2018-11-06 10:48:23 +0100

    Removed image downloading

--- app/controllers/scrambler.js
+++ app/controllers/scrambler.js
@@ -1,21 +1,8 @@
 const Scrambo = require('scrambo');
-const download = require('images-downloader').images;
 const fs = require('fs-extra');
 
 const event333 = async () => {
-  const scrambles = new Scrambo().get(5);
-  const filesList = scrambles.map(s => [
-    'http://roudai.net/visualcube/visualcube.php',
-    '?fmt=png',
-    '&size=90',
-    '&sch=wrgyob',
-    '&alg=',
-    s.replace(/ /g, '').replace(/'/g, '%27')
-  ].join(''));
-  await fs.ensureDir('./tmp');
-  const files = (await download(filesList, './tmp')).map(f => f.filename);
-
-  return {scrambles, files};
+  return {scrambles: new Scrambo().get(5);, files: []};
 };
 
 const sendScrambles = async (chan, header, {scrambles, files}) => {
