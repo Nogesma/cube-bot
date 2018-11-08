e4909704aaa5519d91bbfa3c1077d17343385c8f
ManoSegransan <mano.segransan@gmail.com>
2018-11-08 10:40:42 +0100

    updated tests

--- test/message-controller-test.js
+++ test/message-controller-test.js
@@ -28,7 +28,7 @@ describe.skip('app/controllers/message-controller.js', () => {
       await mongoose.disconnect();
     });
 
-    it('should call insertNewTimes', async () => {
+    it('should call newTimesCommand', async () => {
       await incomingMessage({
         content: '?t 333 12 13 14 15 16',
         channel: {
@@ -39,6 +39,50 @@ describe.skip('app/controllers/message-controller.js', () => {
       });
       stub.should.have.been.calledWith('2018-01-01', 'author', '333',
         ['12', '13', '14', '15', '16']);
+    });
+
+    it('should call helpCommand', async () => {
+      await incomingMessage({
+        content: '?h' || '?help',
+        channel: {
+          send: () => {
+          }
+        }
+      });
+    });
+
+    it('should call dailyRanksCommand', async () => {
+      await incomingMessage({
+        content: '?classement 333 2018-01-01',
+        channel: {
+          send: () => {
+          }
+        }
+      });
+      stub.should.have.been.calledWith('333', '2018-01-01');
+    });
+
+    it('should call monthlyRanksCommand', async () => {
+      await incomingMessage({
+        content: '?classementmois 333 2018-01',
+        channel: {
+          send: () => {
+          }
+        }
+      });
+      stub.should.have.been.calledWith('333', '2018-01');
+    });
+
+    it('should call dididoCommand', async () => {
+      await incomingMessage({
+        content: '?didido 333',
+        channel: {
+          send: () => {
+          }
+        },
+        author: {id: 'author'}
+      });
+      stub.should.have.been.calledWith('author', '333');
     });
   });
 });
