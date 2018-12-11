229b45d966691456ceb52a580bfb4216db1edf01
Nogesma <mano.segransan@gmail.com>
2018-12-12 00:39:54 +0100

    Removed unused wins and podiums from db

--- app/models/rankings.js
+++ app/models/rankings.js
@@ -5,8 +5,6 @@ const rankingsSchema = new mongoose.Schema({
   date: {type: Date, default: Date.now},
   author: {type: String, required: true},
   score: {type: Number, default: 0},
-  wins: {type: Number, default: 0},
-  podiums: {type: Number, default: 0},
   event: {type: String, enum: availableEvents, require: true},
   attendances: {type: Number, default: 0}
 });
