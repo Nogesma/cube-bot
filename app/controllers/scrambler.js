b12c9bdf5e1f941e1bc1d850bfb9cf8cdc598187
Nogesma <mano.segransan@gmail.com>
2019-02-06 13:17:32 +0100

    Fix scramble display on iOS and rankings

--- app/controllers/scrambler.js
+++ app/controllers/scrambler.js
@@ -2,10 +2,11 @@ const R = require('ramda');
 const {Scrambow} = require('scrambow');
 
 const event333 = async () => R.join('\n',
-  R.pluck('scramble_string', new Scrambow().get(5)));
+  R.map(R.slice(0, -1), R.pluck('scramble_string', new Scrambow().get(5))));
 
 const event222 = async () => R.join('\n',
-  R.pluck('scramble_string', new Scrambow().setType('222').get(5)));
+  R.map(R.slice(0, -1), R.pluck('scramble_string',
+    new Scrambow().setType('222').get(5))));
 
 const sendScrambles = (chan, event, date, scrambles) =>
   chan.send(`Scrambles ${event} (${date})\n` + scrambles);
