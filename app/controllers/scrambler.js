b20b1ccf384a33a89acbdc5acc2a2ee61afab1e8
Nogesma <mano.segransan@gmail.com>
2019-02-06 13:30:30 +0100

    Refactor scrambler

--- app/controllers/scrambler.js
+++ app/controllers/scrambler.js
@@ -1,12 +1,17 @@
 const R = require('ramda');
 const {Scrambow} = require('scrambow');
 
-const event333 = async () => R.join('\n',
-  R.map(R.slice(0, -1), R.pluck('scramble_string', new Scrambow().get(5))));
+const event333 = async () => R.pipe(
+  R.pluck('scramble_string'),
+  R.map(R.trim),
+  R.join('\n')
+)(new Scrambow().get(5));
 
-const event222 = async () => R.join('\n',
-  R.map(R.slice(0, -1), R.pluck('scramble_string',
-    new Scrambow().setType('222').get(5))));
+const event222 = async () => R.pipe(
+  R.pluck('scramble_string'),
+  R.map(R.trim),
+  R.join('\n')
+)(new Scrambow().setType('222').get(5));
 
 const sendScrambles = (chan, event, date, scrambles) =>
   chan.send(`Scrambles ${event} (${date})\n` + scrambles);
