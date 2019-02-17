1c05472a8398639f43d369160f492a209a729bfb
Nogesma <mano.segransan@gmail.com>
2019-02-17 19:21:49 +0100

    Users will not be able to submit times between 23h59 and 00h1

--- app/controllers/cube-db.js
+++ app/controllers/cube-db.js
@@ -23,6 +23,10 @@ const insertNewTimes = async ({channel, date, author, event, args: solves}) => {
   if (channel.type !== 'dm') {
     return 'Veuillez envoyer vos temps en message privé';
   }
+  const hour = moment().format('HH:MM');
+  if (hour === '23:59' || hour === '00:00') {
+    return 'Vous ne pouvez pas soumettre vos temps entre 23h59 et 00h01';
+  }
   if (solves.length !== 5) {
     return 'Veuillez entrer 5 temps';
   }
