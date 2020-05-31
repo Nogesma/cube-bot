5e0be469fcfd14a6be68a4f9d2e1117ffebbebfa
Mano Ségransan <mano.segransan@protonmail.com>
2020-05-31 17:49:06 +0200

    Add pyraminx

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const R = require('ramda');
 
 module.exports = {
-  events: ['333', '222', '444', '3BLD', 'OH', 'SQ1', 'MINX'],
+  events: ['333', '222', '444', '3BLD', 'OH', 'SQ1', 'MINX', 'PYRAM'],
   hours: R.range(1, 24),
 };
